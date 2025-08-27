a2a95fef0214292382b33bc98d88b1f91f237608
FaustNFurious <lorelego2003@gmail.com>
2025-08-27 23:06:00 +0200

    mod

--- resources/views/services/ServicesCreation.blade.php
+++ resources/views/services/ServicesCreation.blade.php
@@ -12,7 +12,7 @@
         <div class="row align-items-center justify-content-center">
             <div class="col-12 col-md-6">
 
-                <form class="my-5" method="post" action="{{route('services.Submit')}}">
+                <form class="my-5" method="POST" action="{{route('services.Submit')}}">
                     @csrf
                     <div class="mb-3">
                         <label for="brand" class="form-label">Marchio Prodotto</label>
