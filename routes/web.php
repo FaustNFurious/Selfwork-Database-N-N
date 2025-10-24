906a4218859bfd5d5d34418736ce12c67af5f829
FaustNFurious <lorelego2003@gmail.com>
2025-10-24 15:29:04 +0200

    prova

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\ServicesController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
